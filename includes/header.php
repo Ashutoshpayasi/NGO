678812396c2f717be27d24802b1fe76ecf4855d5
Asutosh Payasi <asutoshpayasi2001@gmail.com>
2024-06-11 08:24:17 +0530

    includes header footer

---
+++ includes/header.php
@@ -0,0 +1,151 @@
+<div class="custom-cursor__cursor"></div>
+    <div class="custom-cursor__cursor-two"></div>
+
+
+
+
+
+    <div class="preloader">
+        <div class="preloader__image"></div>
+    </div>
+    <!-- /.preloader -->
+
+
+    <div class="page-wrapper">
+        <header class="main-header">
+            <nav class="main-menu">
+                <div class="main-menu__wrapper">
+                    <div class="main-menu__wrapper-inner">
+                        <div class="main-menu__left">
+                            <div class="main-menu__logo">
+                                <a href="index.php">
+                                    <img src="assets/images/logo/vnslogofinal.png" alt="">
+                                </a>
+                            </div>
+                            <div class="header-widget__about-text">
+                                VeerNari Shakti <br>
+                                Resettlement Foundation
+                            </div>
+                        </div>
+
+                        <div class="main-menu__right">
+                            <div class="main-menu__right-top">
+                                <div class="main-menu__right-top-left">
+                                    <div class="main-menu__volunteers">
+                                        <div class="main-menu__volunteers-icon">
+                                            <img src="assets/images/icon/main-menu-heart-icon.png" alt="">
+                                        </div>
+                                        <div class="main-menu__volunteers-text-box">
+                                            <p class="main-menu__volunteers-text"><a href="become-volunteer.php">Become
+                                                    a
+                                                    <span>volunteers</span></a></p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="main-menu__right-top-right">
+                                    <div class="main-menu__right-top-address">
+                                        <ul class="list-unstyled main-menu__right-top-address-list">
+                                            <li>
+                                                <div class="icon">
+                                                    <span class="icon-phone-call"></span>
+                                                </div>
+                                                <div class="content">
+                                                    <p>Helpline</p>
+                                                    <h5><a href="tel: 9821022008">+91 9821022008</a></h5>
+                                                </div>
+                                            </li>
+                                            <li>
+                                                <div class="icon">
+                                                    <span class="icon-message"></span>
+                                                </div>
+                                                <div class="content">
+                                                    <p>Send email</p>
+                                                    <h5><a href="mailto:vnsrfoundation@gmail.com">vnsrfoundation@gmail.com</a>
+                                                    </h5>
+                                                </div>
+                                            </li>
+                                            <li>
+                                                <div class="icon">
+                                                    <span class="icon-location"></span>
+                                                </div>
+                                                <div class="content">
+                                                    <p>1-63,CGHS Society Vikaspuri</p>
+                                                    <h5>Jankapuri,Delhi</h5>
+                                                </div>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div class="main-menu__right-top-social">
+                                        <a href="#"><i class="fab fa-twitter"></i></a>
+                                        <a href="#"><i class="fab fa-facebook"></i></a>
+                                        <a href="#"><i class="fab fa-pinterest-p"></i></a>
+                                        <a href="#"><i class="fab fa-instagram"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="main-menu__right-bottom">
+                                <div class="main-menu__main-menu-box">
+                                    <a href="#" class="mobile-nav__toggler"><i class="fa fa-bars"></i></a>
+                                    <ul class="main-menu__list">
+                                        <li class="megamenu current megamenu-clickable megamenu-clickable--toggler">
+                                            <a href="index.php">Home</a>
+
+                                        </li>
+                                        <li class="dropdown">
+                                            <a href="#">About</a>
+                                            <!-- <ul>
+                                                <li><a href="about.php">About</a></li>
+                                                <li><a href="volunteer.php">Volunteer</a></li>
+                                                <li><a href="become-volunteer.php">Become volunteer</a></li>
+                                                <li><a href="volunteer-carousel.php">Volunteer carousel</a></li>
+                                                <li><a href="gallery.php">Gallery</a></li>
+                                                <li><a href="gallery-carousel.php">Gallery carousel</a></li>
+                                                <li><a href="faq.php">FAQs</a></li>
+                                                <li><a href="404.php">404 Error</a></li>
+                                            </ul> -->
+                                        </li>
+                                        <li class="dropdown">
+                                            <a href="#">Contacts</a>
+
+                                        </li>
+                                        <li class="dropdown">
+                                            <a href="#">Events</a>
+                                            <ul>
+                                                <li><a href="events.php">Events</a></li>
+                                                <li><a href="events-carousel.php">Events carousel</a></li>
+                                                <li><a href="event-details.php">Event details</a></li>
+                                            </ul>
+                                        </li>
+
+                                        <li class="dropdown">
+                                            <button class="download-btn" data-url="assets/brochure/GLLPP3239J-2025(1).pdf">Download Brochure</button>
+                                        </li>
+                                        <li>
+                                            <a href="gallery.php">Gallery</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="main-menu__main-menu-content-box">
+                                    <div class="main-menu__search-cat-btn-box">
+                                        <div class="main-menu__search-box">
+                                            <a href="#" class="main-menu__search search-toggler icon-magnifying-glass"></a>
+                                        </div>
+                                        <div class="main-menu__cat-box">
+                                            <a href="cart.php" class="main-menu__cart icon-shopping-cart"></a>
+                                        </div>
+                                        <div class="main-menu__btn-box">
+                                            <a href="https://eazypay.icicibank.com/eazypayLink?P1=wvMjvn/rUQaWiz5GvbP5Jw==" target="_blank" class="main-menu__btn"> <span class="fa fa-heart"></span> Donate
+                                                now</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </header>
+
+        <div class="stricky-header stricked-menu main-menu">
+            <div class="sticky-header__content"></div><!-- /.sticky-header__content -->
+        </div><!-- /.stricky-header -->
